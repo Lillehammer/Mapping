1476dacd22c76f79f0dbcb3325d37ac8e142ded2
Dr. Ignaz M <9mmfullclip@gmail.com>
2016-05-05 22:56:36 +0200

    new base

---
+++ Clans/DVG/Base/copy_from_xcam_to_repo.bat
@@ -0,0 +1,9 @@
+@echo off
+
+copy "D:\Steam\steamapps\common\Arma 3\@inidbi\db\xCam\Library_Pro\_xCamProjects.ini" .
+
+copy "D:\Steam\steamapps\common\Arma 3\@inidbi\db\xCam\Library_Pro\Altis_Stadion2.ini" .
+
+copy "D:\Steam\steamapps\common\Arma 3\mission_Altis_Stadion2.sqm" .
+
+del "D:\Steam\steamapps\common\Arma 3"\mission_*.sqm
